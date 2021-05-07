ed59ec284f04cf3d78d2c0cfd31b5963cc656b6f
Leonardo Freire Russi <leonardo.russi.freire@gmail.com>
2021-05-07 17:16:07 -0300

    .gitignore atualizado e adicionado input para o usuario

--- conexao_db.py
+++ conexao_db.py
@@ -1,9 +1,13 @@
 #importando pymongo
 from pymongo import MongoClient
 from api import showUserInfo
+from pprint import pprint
 
-data_api = showUserInfo('whoisfreire')
-user = data_api.return_info()
+user = str(input('Usuário: '))
+
+data_api = showUserInfo(user)
+user_info = data_api.return_info()
+
 #conectando 
 client = MongoClient("localhost", 27017)
 
@@ -11,12 +15,15 @@ db = client.githubUsers
 
 db.usuarios.insert_one(
     {
-        "nome": user['login'],
-        "avatar": user['avatar_url'],
-        "perfil": user['html_url'],
-        "followers": user['followers'],
-        "following": user['following'],
-        "public_repositories": user['public_repos'],
-        "twitter": user['twitter_username']
+        "nome": user_info['login'],
+        "avatar": user_info['avatar_url'],
+        "perfil": user_info['html_url'],
+        "followers": user_info['followers'],
+        "following": user_info['following'],
+        "public_repositories": user_info['public_repos'],
+        "twitter": user_info['twitter_username']
     }
 )
+
+for user  in db.usuarios.find():
+    pprint(user)
