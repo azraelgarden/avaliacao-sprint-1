9b169264d31f052047bb32f943b35d814a018cdb
lucashtdesa <lucas.htdesa@gmail.com>
2021-05-10 01:51:01 -0300

    Adicionando uma funcao de já vistos

--- ip.py
+++ ip.py
@@ -1,39 +1,49 @@
 import urllib.request as urllib2
 import json
-
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client.trabalhoAvaliativo
 
 
-# Caso queira testar alguns ips, esses sao validos
-# 24.55.21.33, 2606:4700:10::6816:3866
-# 52.211.131.50, 13.94.143.57
+# Caso queira testar algum ip, estes sao válidos
+# 24.55.21.33, 2606:4700:10::6816:3866, 15.44.56.9, 43.27.4.8
+# 52.211.131.50, 13.94.143.57, 12.53.21.2, 32.12.66.4, 77.54.25.3
+
 
 def get_ip():
     ip = input("Digite um IP: ")
-    response = urllib2.urlopen("http://ipwhois.app/json/" + ip)
+    response = urllib2.urlopen("http://ipwhois.app/json/{}?lang=pt-BR".format(ip))
     ipgeolocation = json.load(response)
-
     if ipgeolocation["success"]:
         findone = db.ip.find_one({"ip": ipgeolocation["ip"]})
         if findone:
-            print("ja tem no banco")
+            print("Este IP ja tem no banco")
             ipgeolocation = findone
             menu(ipgeolocation)
         else:
-            print("nao tinha no banco")
+            print("Adicionando esse ip no banco...")
             db.ip.insert_one(ipgeolocation)
             menu(ipgeolocation)
     else:
         print("Esse IP não é válido! Tente novamente")
         get_ip()
 
-def jaVistos():
 
+def jaVistos(ipgeolocation):
+    print("Aqui estão os IPs já vistos: ")
+    for x in db.ip.find({},{"ip"}):
+        print(x["ip"])
 
-    pass
+    escolha = input("Escolha um IP acima ou digite 0 para sair: ")
+    if escolha == db.ip.find_one({"ip": escolha}):
+        ipgeolocation = db.ip.find_one({"ip": escolha})
+        menu(ipgeolocation)
+    elif escolha == "0":
+        raise SystemExit
+    else:
+        print("Este IP não está na lista a cima... Tente Novamente")
+        jaVistos(ipgeolocation)
 
 def menu(ipgeolocation):
     if False:
@@ -42,13 +52,14 @@ def menu(ipgeolocation):
         opcao = 0
         while opcao != 7:
 
-            print('''O IP {} é válido, O que deseja ver sobre ele?
+            print('''-=-=-=-=-=-=-=-=-=-
+O que deseja ver sobre ele?
 1...Local
 2...Dados geograficos
 3...Moeda
 4...Tudo
 5...Novo IP
-6...Historico de buscas de IP
+6...Ips já 
 7...Para sair
 -=-=-=-=-=-=-=-=-=-'''.format(ipgeolocation["ip"]))
             opcao = int(input("Digite a opção: "))
@@ -56,10 +67,10 @@ def menu(ipgeolocation):
             if opcao == 1:
                 print('''
 -=-=-=-=-=-=-=-=-=-
-Cidade: {}, Estado: {}, País: {}, Capital: {}
+Cidade: {}, Estado: {}, País: {}, Capital: {}, Bandeira do País: {}
 -=-=-=-=-=-=-=-=-=-'''
                       .format(ipgeolocation["city"], ipgeolocation["region"], ipgeolocation["country"],
-                              ipgeolocation["country_capital"]))
+                              ipgeolocation["country_capital"], ipgeolocation["country_flag"]))
 
             elif opcao == 2:
                 print('''
@@ -91,13 +102,14 @@ Latitude: {}, Longitude: {}, Moeda: {}, Codigo: {}, Símbolo: {}, Plural: {}.
                               ipgeolocation["currency_plural"]))
 
             elif opcao == 5:
-                historicoIP(ipgeolocation)
-                #get_ip()
+                get_ip()
 
+            elif opcao == 6:
+                jaVistos(ipgeolocation)
 
             elif opcao == 7:
                 print("Saindo...")
-                return False
+                raise SystemExit
 
             else:
                 print('''
@@ -105,6 +117,12 @@ Latitude: {}, Longitude: {}, Moeda: {}, Codigo: {}, Símbolo: {}, Plural: {}.
 Opção invalida
 -=-=-=-=-=-=-=-=-=-
             ''')
+
+
+
+
+
+
 
 
 if True:
